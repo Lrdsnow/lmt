20e78c9f5edb632638879a81aa24029c96c22522
Lrdsnow <Lrdsnow101@gmail.com>
2023-05-18 18:51:32 -0700

    c++

--- lmt.cpp
+++ lmt.cpp
@@ -2,229 +2,170 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <algorithm>
-#include <iterator>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <cstdlib>
-#include <cstdio>
-#include <string>
-#include <vector>
 #include <cstring>
-#include <dirent.h>
-#include "deps/json.hpp"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <curl/curl.h>
+#include <json/json.h>
+#include <filesystem>  // Add this line for std::filesystem support
+
+namespace fs = std::filesystem;  // Add this line for namespace alias
 
 // Variables
 int verbose = 0;
 std::string flags = "-q";
-std::string home = std::string(getenv("HOME")) + "/.lmt";
-
-using json = nlohmann::json;
+std::string home = getenv("HOME") + std::string("/.lmt");
 
 // Basic Setup
 void setup() {
-    mkdir(home.c_str(), 0755);
-    mkdir((home + "/bin").c_str(), 0755);
-    mkdir((home + "/temp").c_str(), 0755);
+    mkdir(home.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    mkdir((home + "/bin").c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    mkdir((home + "/temp").c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 }
 
 // Repository's
 void update() {
     std::string repos_conf_path = home + "/config/repos.json";
-    std::ifstream repos_conf_file(repos_conf_path);
-    if (!repos_conf_file.good()) {
-        mkdir((home + "/config").c_str(), 0755);
-        mkdir((home + "/repos").c_str(), 0755);
-        std::ofstream new_repos_conf_file(repos_conf_path);
-        new_repos_conf_file << R"(
-{
-    "repos": ["https://raw.githubusercontent.com/Lrdsnow/lmt/main/repo"],
-    "cpkgs": []
-}
-)";
-        new_repos_conf_file.close();
-        std::string path_env = std::string(getenv("PATH"));
-        if (path_env.find(home + "/bin") == std::string::npos) {
-            std::ofstream bashrc_file(std::string(getenv("HOME")) + "/.bashrc", std::ios::app);
-            bashrc_file << "export PATH=\"$PATH:" << home << "/bin\"\n";
-            bashrc_file.close();
+    if (access(repos_conf_path.c_str(), F_OK) != 0) {
+        mkdir((home + "/config").c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        mkdir((home + "/repos").c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        Json::Value data;
+        data["repos"].append("https://raw.githubusercontent.com/Lrdsnow/lmt/main/repo");
+        data["cpkgs"] = Json::Value(Json::arrayValue);
+        std::ofstream file(repos_conf_path);
+        file << data;
+        file.close();
+        if (std::string(getenv("PATH")).find(home + "/bin") == std::string::npos) {
+            std::ofstream bashrcFile(getenv("HOME") + std::string("/.bashrc"), std::ios_base::app);
+            bashrcFile << "export PATH=\"$PATH:" << home << "/bin\"\n";
+            bashrcFile.close();
         }
     }
-    std::ifstream repos_conf_input(repos_conf_path);
-    std::stringstream repos_conf_ss;
-    repos_conf_ss << repos_conf_input.rdbuf();
-    std::string repos_conf_data = repos_conf_ss.str();
-    repos_conf_input.close();
-
-    json repos_conf_json = json::parse(repos_conf_data);
-    std::vector<std::string> repos = repos_conf_json["repos"];
-
-    if (repos.empty()) {
+    std::ifstream file(repos_conf_path);
+    Json::Value data;
+    file >> data;
+    file.close();
+    Json::Value repos = data["repos"];
+    if (repos.size() == 0) {
         std::cout << "Failed, No repositories available" << std::endl;
         exit(1);
     }
-
     for (const auto& src : repos) {
         std::cout << "Checking " << src << "/repo.json..." << std::endl;
-        std::string repo_json_url = src + "/repo.json";
-        std::string temp_repo_json_path = home + "/temp/repo.json";
-        std::string wget_command = "wget \"" + repo_json_url + "\" " + flags + " -O " + temp_repo_json_path;
-
-        int wget_return_code = system(wget_command.c_str());
-        if (wget_return_code == 0) {
-            std::ifstream repo_json_file(temp_repo_json_path);
-            std::stringstream repo_json_ss;
-            repo_json_ss << repo_json_file.rdbuf();
-            std::string repo_json_data = repo_json_ss.str();
-            repo_json_file.close();
-
-            std::ofstream repo_json_dest(home + "/repos/" + repo_json_data + ".json");
-            repo_json_dest << repo_json_data;
-            repo_json_dest.close();
-
+        std::string repo_json_url = src.asString() + "/repo.json";
+        std::string temp_repo_json = home + "/temp/repo.json";
+        std::string wget_command = "wget " + flags + " -O " + temp_repo_json + " " + repo_json_url;
+        if (system(wget_command.c_str()) == 0) {
+            std::ifstream repoFile(temp_repo_json);
+            Json::Value repo_data;
+            repoFile >> repo_data;
+            repoFile.close();
+            std::string repo_name = repo_data["name"].asString();
+            std::string dest_repo_json = home + "/repos/" + repo_name + ".json";
+            rename(temp_repo_json.c_str(), dest_repo_json.c_str());
             std::cout << "Successfully downloaded repository file" << std::endl;
         } else {
             std::cout << "Failed to download repository file" << std::endl;
         }
     }
-
-    std::string repos_dir = home + "/repos";
-    DIR* dir;
-    struct dirent* ent;
-    if ((dir = opendir(repos_dir.c_str())) != nullptr) {
-        while ((ent = readdir(dir)) != nullptr) {
-            std::string repo_filename = ent->d_name;
-            if (repo_filename.find(".json") != std::string::npos) {
-                std::cout << "Found " << repo_filename << std::endl;
-                std::ifstream repo_file(home + "/repos/" + repo_filename);
-                std::stringstream repo_ss;
-                repo_ss << repo_file.rdbuf();
-                std::string repo_data = repo_ss.str();
-                repo_file.close();
-
-                std::ifstream pkgs_data_file(repos_conf_path);
-                std::stringstream pkgs_data_ss;
-                pkgs_data_ss << pkgs_data_file.rdbuf();
-                std::string pkgs_data = pkgs_data_ss.str();
-                pkgs_data_file.close();
-
-                std::string cpkgs = pkgs_data.substr(pkgs_data.find("\"cpkgs\": [") + 11);
-                cpkgs = cpkgs.substr(0, cpkgs.find_first_of("]"));
-                cpkgs.erase(std::remove_if(cpkgs.begin(), cpkgs.end(), ::isspace), cpkgs.end());
-
-                std::string pkgs = repo_data.substr(repo_data.find("\"pkgs\": [") + 10);
-                pkgs = pkgs.substr(0, pkgs.find_first_of("]"));
-                pkgs.erase(std::remove_if(pkgs.begin(), pkgs.end(), ::isspace), pkgs.end());
-
-                pkgs_data.replace(pkgs_data.find("\"cpkgs\": [") + 11, cpkgs.length(), cpkgs + pkgs);
-
-                std::ofstream pkgs_data_dest(repos_conf_path);
-                pkgs_data_dest << pkgs_data;
-                pkgs_data_dest.close();
-
-                std::cout << "Successfully Refreshed Repo '" << repo_data << "'" << std::endl;
-            } else if (verbose) {
-                std::cout << "Skipping " << repo_filename << std::endl;
+    for (const auto& repo : std::filesystem::directory_iterator(home + "/repos")) {
+        std::string repo_filename = repo.path().filename().string();
+        if (repo_filename.find(".json") != std::string::npos) {
+            std::cout << "Found " << repo_filename << std::endl;
+            std::ifstream repoFile(repo.path());
+            Json::Value repo_data;
+            repoFile >> repo_data;
+            repoFile.close();
+            std::ifstream pkgsFile(home + "/config/repos.json");
+            Json::Value pkgs_data;
+            pkgsFile >> pkgs_data;
+            pkgsFile.close();
+            Json::Value cpkgs = pkgs_data.get("cpkgs", Json::Value(Json::arrayValue));
+            Json::Value pkgs = repo_data.get("pkgs", Json::Value(Json::arrayValue));
+            pkgs_data["cpkgs"] = cpkgs;
+            for (const auto& pkg : pkgs) {
+                pkgs_data["cpkgs"].append(pkg);
             }
+            std::ofstream outfile(home + "/config/repos.json");
+            outfile << pkgs_data;
+            outfile.close();
+            std::cout << "Successfully Refreshed Repo '" << repo_data["name"].asString() << "'" << std::endl;
+        } else if (verbose) {
+            std::cout << "Skipping " << repo_filename << std::endl;
         }
-        closedir(dir);
     }
 }
 
-// Packages
-
-
 void install_package(const std::string& package) {
-    mkdir((home + "/temp/unpkged").c_str(), 0755);
+    mkdir((home + "/temp/unpkged").c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     std::string unzip_command = "unzip " + flags + " " + package + " -d " + home + "/temp/unpkged/";
     system(unzip_command.c_str());
-
-    std::string cwd = get_current_dir_name();
+    std::string cwd = getcwd(NULL, 0);
     chdir((home + "/temp/unpkged/").c_str());
-
-    std::string preinst_command = "bash preinst.sh";
-    system(preinst_command.c_str());
-
-    std::ifstream info_json_file("info.json");
-    std::stringstream info_json_ss;
-    info_json_ss << info_json_file.rdbuf();
-    std::string info_json_data = info_json_ss.str();
-    info_json_file.close();
-
-    std::string pkg_name = info_json_data;
-
-    std::string install_command = "sudo cp -R bin/ /usr/local/";
-    system(install_command.c_str());
-
-    std::string postinst_command = "bash postinst.sh";
-    system(postinst_command.c_str());
-
-    std::string cleanup_command = "sudo rm -rf /usr/local/bin/" + pkg_name;
-    system(cleanup_command.c_str());
-
+    std::string preinst_script = "bash preinst.sh";
+    system(preinst_script.c_str());
+    std::ifstream infoFile("info.json");
+    Json::Value info_data;
+    infoFile >> info_data;
+    infoFile.close();
+    std::string name = info_data["name"].asString();
+    std::string version = info_data["version"].asString();
+    std::cout << "Installing " << name << "@" << version << "..." << std::endl;
+    std::string inst_script = "bash inst.sh";
+    if (system(inst_script.c_str()) == 0) {
+        std::cout << "Successfully installed " << name << "@" << version << std::endl;
+    } else {
+        std::cout << "Failed to install " << name << std::endl;
+    }
     chdir(cwd.c_str());
     system(("rm -rf " + home + "/temp/unpkged").c_str());
-
-    std::cout << "Successfully installed " << package << std::endl;
 }
 
 bool search_package(const std::string& package) {
     std::string pkgs_file = home + "/config/repos.json";
-    std::ifstream pkgs_file_input(pkgs_file);
-    std::stringstream pkgs_file_ss;
-    pkgs_file_ss << pkgs_file_input.rdbuf();
-    std::string pkgs_file_data = pkgs_file_ss.str();
-    pkgs_file_input.close();
-
-    std::istringstream iss(pkgs_file_data);
-    std::string line;
-    while (std::getline(iss, line)) {
-        if (line.find("\"cpkgs\": [") != std::string::npos) {
-            std::istringstream cpkgs_line_ss(line);
-            std::string cpkg;
-            while (std::getline(cpkgs_line_ss, cpkg, ',')) {
-                if (cpkg.find(package) != std::string::npos) {
-                    return true;
-                }
+    if (access(pkgs_file.c_str(), F_OK) == 0) {
+        std::ifstream file(pkgs_file);
+        Json::Value data;
+        file >> data;
+        file.close();
+        Json::Value cpkgs = data.get("cpkgs", Json::Value(Json::arrayValue));
+        for (const auto& cpkg : cpkgs) {
+            if (cpkg.asString() == package) {
+                return true;
             }
         }
     }
-
     std::cout << "Failed, No packages found!" << std::endl;
     return false;
 }
 
 bool download_package(const std::string& package) {
-    std::string repos_dir = home + "/repos";
-    DIR* dir;
-    struct dirent* ent;
-    if ((dir = opendir(repos_dir.c_str())) != nullptr) {
-        while ((ent = readdir(dir)) != nullptr) {
-            std::string repo_filename = ent->d_name;
-            if (repo_filename.find(".json") != std::string::npos) {
-                std::ifstream repo_file(home + "/repos/" + repo_filename);
-                std::stringstream repo_ss;
-                repo_ss << repo_file.rdbuf();
-                std::string repo_data = repo_ss.str();
-                repo_file.close();
-
-                if (repo_data.find(package) != std::string::npos) {
-                    std::string package_url = repo_data + "/pkgs/" + package + ".lmt";
-                    std::string temp_package_path = home + "/temp/" + package + ".lmt";
-                    std::string wget_command = "wget \"" + package_url + "\" " + flags + " --show-progress -O " + temp_package_path;
-
-                    int wget_return_code = system(wget_command.c_str());
-                    if (wget_return_code == 0) {
+    for (const auto& repo : std::filesystem::directory_iterator(home + "/repos")) {
+        std::string repo_filename = repo.path().filename().string();
+        if (repo_filename.find(".json") != std::string::npos) {
+            std::ifstream repoFile(repo.path());
+            Json::Value repo_data;
+            repoFile >> repo_data;
+            repoFile.close();
+            Json::Value pkgs = repo_data["pkgs"];
+            for (const auto& pkg : pkgs) {
+                if (pkg.asString() == package) {
+                    std::string package_url = repo_data["url"].asString() + "/pkgs/" + package + ".lmt";
+                    std::string temp_package_file = home + "/temp/" + package + ".lmt";
+                    std::string wget_command = "wget " + flags + " --show-progress -O " + temp_package_file + " " + package_url;
+                    if (system(wget_command.c_str()) == 0) {
                         return true;
                     } else {
                         return false;
                     }
                 }
-            } else if (verbose) {
-                std::cout << "Skipping " << repo_filename << std::endl;
             }
+        } else if (verbose) {
+            std::cout << "Skipping " << repo_filename << std::endl;
         }
-        closedir(dir);
     }
     return false;
 }
@@ -237,7 +178,7 @@ void install(const std::vector<std::string>& args) {
                 if (p.find(".deb") == std::string::npos) {
                     install_package(p);
                 } else {
-                    std::string dpkg_command = "sudo dpkg install " + p;
+                    std::string dpkg_command = "sudo dpkg -i " + p;
                     system(dpkg_command.c_str());
                 }
             } else {
@@ -251,9 +192,8 @@ void install(const std::vector<std::string>& args) {
                     std::cout << "Failed, Download failed" << std::endl;
                 }
             } else {
-                std::string apt_search_command = "apt search \"^" + p + "$\" -qq";
-                int apt_search_return_code = system(apt_search_command.c_str());
-                if (apt_search_return_code == 0) {
+                std::string apt_search_command = "apt search ^" + p + "$ -qq";
+                if (system(apt_search_command.c_str()) == 0) {
                     std::string apt_install_command = "sudo apt install " + p;
                     system(apt_install_command.c_str());
                 } else {
@@ -264,11 +204,43 @@ void install(const std::vector<std::string>& args) {
     }
 }
 
+// Usage
+void print_usage() {
+    std::cout << "install (-i): Install Package(s)" << std::endl;
+    std::cout << "update (-u): Update/Refreshes Repositories" << std::endl;
+    std::cout << "help (-h): Displays This Help Message" << std::endl;
+    std::cout << "-v: Verbose mode" << std::endl;
+}
+
+// Grab flags
+std::vector<std::string> parse_arguments(int argc, char* argv[]) {
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; i++) {
+        std::string flag = argv[i];
+        if (flag == "-i") {
+            i++;
+            while (i < argc) {
+                args.push_back(argv[i]);
+                i++;
+            }
+            install(args);
+        } else if (flag == "-u") {
+            update();
+        } else if (flag == "-h") {
+            print_usage();
+        } else if (flag == "-v") {
+            verbose = 1;
+            flags = "";
+        }
+    }
+    return args;
+}
+
 int main(int argc, char* argv[]) {
     setup();
 
     if (argc < 2) {
-        std::cout << "No command specified" << std::endl;
+        print_usage();
         return 0;
     }
 
@@ -282,8 +254,12 @@ int main(int argc, char* argv[]) {
         }
         install(args);
     } else {
-        std::cout << "Invalid command" << std::endl;
+        std::vector<std::string> args = parse_arguments(argc, argv);
+        if (args.empty()) {
+            print_usage();
+        }
     }
 
     return 0;
 }
+
